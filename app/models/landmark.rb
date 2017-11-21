0e63011d37baeec3f3bcb0f2568af7551a1398cb
Oscar Edel <edeloscar@gmail.com>
2017-11-21 18:30:26 +0100

    adds some initial map code

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -2,6 +2,13 @@ class Landmark < ApplicationRecord
   has_many :stories
   validates :name, uniqueness: :true, presence: :true
   validates :image, presence: :true
-  validates :lat, presence: :true
-  validates :lng, presence: :true
+  # validates :lat, presence: :true
+  # validates :lng, presence: :true
+
+  geocoded_by :address, :latitude  => :lat, :longitude => :lng
+  after_validation :geocode, if: :address_changed?
+
+  # def address
+  #   [:address, :postal_code, :city, :country].compact.join(', ')
+  # end
 end
