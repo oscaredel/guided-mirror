6f41d1e9cdcf1c5eb15e2d56d700f3fe78a59fba
Dieter Schalk <gargamelk@hotmail.com>
2017-11-21 18:33:18 +0100

    Add stories to story page and home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @stories = Story.first(3)
+    @landmarks = Landmark.first(4)
   end
 end
