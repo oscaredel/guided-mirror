94f471da20fd0516b41b6050473ea7ce1fc21e9e
Oscar Edel <edeloscar@gmail.com>
2017-11-21 11:25:24 +0100

    fixes routes and centers background

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
   get 'about', to: 'pages#about'
 
-  resources :users, only [:show]
-  resources :landmarks, only [:index, :show]
+  resources :users, only: [:show]
+  resources :landmarks, only: [:index, :show]
 end
