5a5134092f529ba781885fb1df89bd5a778a836d
Oscar Edel <edeloscar@gmail.com>
2017-11-22 10:16:19 +0100

    Randomized homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @stories = Story.first(3)
-    @landmarks = Landmark.first(4)
+    @stories = Story.all.sample(3)
+    @landmarks = Landmark.all.sample(4)
   end
 end
