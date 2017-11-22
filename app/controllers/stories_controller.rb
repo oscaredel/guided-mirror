35737b85383a017155e568ae525fbdb3578d94e6
Dieter Schalk <gargamelk@hotmail.com>
2017-11-22 16:56:17 +0100

    Finished add new story page

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -7,4 +7,23 @@ class StoriesController < ApplicationController
     @story = Story.find_by_id(params[:id])
   end
 
+  def new
+    @user = User.find(current_user.id)
+    @story = Story.new
+  end
+
+  def create
+    @user = current_user
+    @story = Story.new(story_params)
+    @story.user = @user
+    if @story.save
+      redirect_to story_path(@story)
+    else
+      render :new
+    end
+  end
+
+  def story_params
+    params.require(:story).permit(:title, :description, :image, :image_cache, :landmark_id)
+  end
 end
