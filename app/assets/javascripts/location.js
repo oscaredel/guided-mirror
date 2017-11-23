c86db6e55c7d612485ee57e9c22e82b0219719f4
Dieter Schalk <gargamelk@hotmail.com>
2017-11-23 17:58:27 +0100

    safari button test

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -6,8 +6,23 @@ function getLocation() {
     }
 }
 
+function enableLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(enablePosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+
 function showPosition(position) {
     window.location = '/?coord[]='+position.coords.latitude+'&coord[]='+position.coords.longitude;
 }
 
+function enablePosition(position) {
+    console.log(position)
+}
 
+
+$(function() {
+  enableLocation();
+})
