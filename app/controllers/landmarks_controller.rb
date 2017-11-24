83483d87c36e1ff38ad59416ac236f98c3187037
Dieter Schalk <gargamelk@hotmail.com>
2017-11-24 16:37:24 +0100

    fixed some css

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -19,5 +19,16 @@ class LandmarksController < ApplicationController
   def show
     @landmark = Landmark.find_by_id(params[:id])
     @stories = @landmark.stories
+    @markers = Gmaps4rails.build_markers(@landmark) do |landmark, marker|
+      marker.lat landmark.lat
+      marker.lng landmark.lng
+      marker.infowindow "<div class='marker-card'> <a class='marker-link' href='#{landmark_path(landmark)}'>
+      <br> <p class='marker-text'>#{landmark.name}</p> </a>
+      </div>"
+      marker.picture({
+        width: 32,
+        height: 32
+        })
+    end
   end
 end
