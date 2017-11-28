a5ebd76221e136a974fe6c98d9698b6a869da555
Dieter Schalk <gargamelk@hotmail.com>
2017-11-28 12:50:36 +0100

    Update form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    edit_user_path(current_user)
+    edit_user_registration_path(current_user)
   end
 end
