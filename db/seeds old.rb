5ad5003c87de251d670ac109f280ba57df0152fd
Dieter Schalk <gargamelk@hotmail.com>
2017-11-30 17:14:35 +0100

    first try at seeds

--- db/seeds old.rb
+++ db/seeds old.rb
@@ -80,88 +80,6 @@ kitty = User.create!(
   description: "I love cooking and can tell you everything about the food in Amsterdam (It's not good).."
   )
 
-dieter = User.create!(
-  first_name: "Dieter",
-  last_name: "Schalk",
-  email: "dieter@gmail.com",
-  password: '123456',
-  remote_image_url: "https://avatars2.githubusercontent.com/u/16388834?s=460&v=4",
-  city: "Nijmegen"
-  description: "Coding student in Amsterdam @Le Wagon. I like dinosaurs."
-  )
-
-bennie = User.create!(
-  first_name: "Bennie",
-  last_name: "",
-  email: "Bennie@gmail.com",
-  password: '123456',
-  remote_image_url: "http://i.dailymail.co.uk/i/pix/2007/08_01/davewestES_468x383.jpg",
-  city: "Amsterdam"
-  description: "I work at the oldest bar of Amsterdam. I could write a book about all that happens here, but I don't want to. Instead, I'll tell you about it here. Come visit our bar sometime."
-  )
-
-feiko = User.create!(
-  first_name: "Feiko",
-  last_name: "",
-  email: "feiko@gmail.com",
-  password: '123456',
-  remote_image_url: "https://photos-2.dropbox.com/t/2/AABVakf7jgTzqYp-tmuuGGm3HsCobpTyb4DTuJ4tJw8u4g/12/1123338/jpeg/32x32/3/1512075600/0/2/_DSF1014.jpg/EI-Qfhj46rsuIAIoAg/02-4O190yk7BOCcgN1Dcw92o8KI56nb81VGIrFmnjM4?dl=0&size=2048x1536&size_mode=3",
-  city: "Amsterdam"
-  description: "Driver Le Wagon Netherlands"
-  )
-
-# https://cdn.diffordsguide.com/contrib/stock-images/2016/9/28/2016f2325ea30a9ad3f3bbf5459dcdcde7cf.jpg
-
- priest = User.create!(
-  first_name: "Priest",
-  last_name: "Frederiks",
-  email: "priest@gmail.com",
-  password: '123456',
-  remote_image_url: "http://www.arjanbroers.nl/wp-content/uploads/2017/02/MoutBierfestival4.jpg",
-  city: "Amsterdam"
-  description: "Priest in Amsterdam. I have some stories to share about God, life, and beer. Yes, beer."
-  )
-
-sanne = User.create!(
-  first_name: "Sanne",
-  last_name: "Jansen",
-  email: "sanne@gmail.com",
-  password: '123456',
-  remote_image_url: "https://ak3.picdn.net/shutterstock/videos/12665933/thumb/1.jpg",
-  city: "Amsterdam"
-  description: "I'm a student of Journalism at the UvA. I love hearing people's stories so I try to interview interesting people I meet in Amsterdam"
-  )
-
-piet = User.create!(
-  first_name: "Piet",
-  last_name: " ",
-  email: "piet@gmail.com",
-  password: '123456',
-  remote_image_url: "http://www.consultancy.nl/media/Kapitein-van-een-schip-20168.jpg",
-  city: "Amsterdam"
-  description: "Captain of a ship for over 30 years now. Based in Amsterdam"
-  )
-
-mia = User.create!(
-  first_name: "Mia",
-  last_name: "",
-  email: "mia@gmail.com",
-  password: '123456',
-  remote_image_url: "https://images.pexels.com/photos/185517/pexels-photo-185517.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb",
-  city: "Amsterdam"
-  description: "I work in the red-light district in Amsterdam. Ever wanted to know what that's like? Listen to my stories."
-  )
-
-dwight = User.create!(
-  first_name: "Dwight",
-  last_name: "",
-  email: "dwight@gmail.com",
-  password: '123456',
-  remote_image_url: "https://www.funx.nl/images/2016/11/03_70_dwight.png",
-  city: "Amsterdam"
-  description: "Police officer. Amsterdam. Indische Buurt."
-  )
-
 # ____Landmarks___________________________________________
 puts "Seeding locations..."
 
@@ -321,17 +239,17 @@ schiphol = Landmark.create!(
   remote_image_url: "http://www.travelvalley.nl/uploads/headers/tips-drukte-schiphol.jpg"
   )
 
-# palace = Landmark.create!(
-#   name: "Imperial Palace",
-#   # lat: 52.3718,
-#   # lng: 4.8969,
-#   description: "This site with scenic gardens & tours of the grounds is the main residence of the emperor of Japan.",
-#   city: "Tokyo",
-#   postal_code: "〒100-8111",
-#   address: "Chiyoda-ku, Chiyoda, 1−",
-#   country: "Japan",
-#   remote_image_url: "https://gaijinpot.scdn3.secure.raxcdn.com/wp-content/uploads/sites/6/2016/07/Tokyo-Imperial-Palace.jpg"
-#   )
+palace = Landmark.create!(
+  name: "Imperial Palace",
+  # lat: 52.3718,
+  # lng: 4.8969,
+  description: "This site with scenic gardens & tours of the grounds is the main residence of the emperor of Japan.",
+  city: "Tokyo",
+  postal_code: "〒100-8111",
+  address: "Chiyoda-ku, Chiyoda, 1−",
+  country: "Japan",
+  remote_image_url: "https://gaijinpot.scdn3.secure.raxcdn.com/wp-content/uploads/sites/6/2016/07/Tokyo-Imperial-Palace.jpg"
+  )
 
 alab = Landmark.create!(
   name: "A-Lab",
@@ -344,79 +262,6 @@ alab = Landmark.create!(
   country: "the Netherlands",
   remote_image_url: "https://www.heijmans.nl/media/filer_public_thumbnails/filer_public/30/3c/303cd39c-2047-451c-9f75-b15054d80d45/ahk-grootlab.jpg__1200x0_q49_subsampling-2.jpg"
   )
-
-portuguese_synagogue = Landmark.create!(
-  name: "Portuguese Synagogue",
-  # lat: 52.3718,
-  # lng: 4.8969,
-  description: "The Portuguese Synagogue, also known as the Esnoga, or Snoge, is a late 17th-century Sephardic synagogue in Amsterdam, completed in 1675. Esnoga is the Ladino word for synagogue.",
-  city: "Amsterdam",
-  postal_code: "1011 RD",
-  address: "Mr. Visserplein 3",
-  country: "the Netherlands",
-  remote_image_url: "https://www.rm-auctions.com/attachment-cache/width/1000/2017/06/02/b21f140/bernard-picart-inauguration-of-the-portuguese-synagogue-in-amsterdam-hand-colored-copper-engraving-1724-1737-1.jpg"
-  )
-
-wallen = Landmark.create!(
-  name: "De Wallen",
-  # lat: 52.3718,
-  # lng: 4.8969,
-  description: "De Wallen is the medieval city center, crossed by canals and narrow alleys lined with old-school bars. Surrounded by the neon-lit red-light district, the Oude Kerk is a centuries-old former church hosting cultural events. Similarly, the old red-brick stock exchange, Beurs van Berlage, now presents exhibitions and concerts. The Museum Ons’ Lieve Heer Op Solder hides an ornate Catholic church in its 17th-century attic.",
-  city: "Amsterdam",
-  postal_code: "1012 DP",
-  address: "Oudezijds Achterburgwal 60h",
-  country: "the Netherlands",
-  remote_image_url: "https://cdn.getyourguide.com/img/tour_img-501798-148.jpg"
-  )
-
-aepjen = Landmark.create!(
-  name: "Café Int Aepjen",
-  # lat: 52.3718,
-  # lng: 4.8969,
-  description: "Housed in an old building dating back to 1475, the Café Int Aepjen is the perfect mix of history and pleasure. This is in fact one of the oldest wooden houses in Amsterdam, furnished with beautiful wooden Interior. Don't forget to enjoy a liquor or gin distillery Van Wees, lo Schoot-An. If you're not in the mood for a drink, You can order a slice of Apple Pie and pair it with a cup of hot coffee. The Café Int Aepjen It is really an experience to try in Amsterdam.",
-  city: "Amsterdam",
-  postal_code: "1012 AN",
-  address: "Zeedijk 1",
-  country: "the Netherlands",
-  remote_image_url: "http://img2.10bestmedia.com/Images/Photos/222505/p-3044906112-1ffb1a1395_54_990x660_201406020322.jpg"
-  )
-
-indische_buurt = Landmark.create!(
-  name: "Indische buurt",
-  # lat: 52.3718,
-  # lng: 4.8969,
-  description: "Multicultural Indische Buurt is an up-and-coming dockside area where hip eateries occupy converted industrial spaces, and late-1800s buildings house Surinamese restaurants, Moroccan supermarkets and Turkish bakeries. Flevopark, a popular picnic location overlooking Nieuwe Diep Lake, has a pond, jogging trails and a distillery bar in a former pump house, with a waterfront terrace.",
-  city: "Amsterdam",
-  postal_code: "1095 CJ",
-  address: "Javaplein 21",
-  country: "the Netherlands",
-  remote_image_url: "https://farm6.staticflickr.com/5590/14697281575_8ba75213ef_o.jpg"
-  )
-
-port_amsterdam = Landmark.create!(
-  name: "Port of Amsterdam",
-  # lat: 52.3718,
-  # lng: 4.8969,
-  description: "The port of Amsterdam is a seaport in Amsterdam, the Netherlands. It is the 4th busiest port in Europe by metric tonnes of cargo.",
-  city: "Amsterdam",
-  postal_code: "Lipariweg 1",
-  address: "1044 AM",
-  country: "the Netherlands",
-  remote_image_url: "https://www.sail.nl/media/85590/luchtfotolfh-260514081_content_page_background_image_crop.jpg"
-  )
-
-victors_house = Landmark.create!(
-  name: "Victor's House",
-  # lat: 52.3718,
-  # lng: 4.8969,
-  description: "The place to be",
-  city: "Amsterdam",
-  postal_code: "Keizersgracht 33",
-  address: "1015 CD",
-  country: "the Netherlands",
-  remote_image_url: "https://cloud.funda.nl/valentina_media/076/587/630_1440x960.jpg"
-  )
-
 
 # ____Stories___________________________________________
 puts "Seeding stories..."
@@ -499,149 +344,6 @@ film_stories = Story.create!(
   lng:4.8852
   )
 
-clown_story = Story.create!(
-  user: bennie,
-  landmark: aepjen,
-  title: "What happens when 3 drunk tourists and a couple of clowns walk into your bar",
-  remote_image_url:  "http://pipelinetheatre.org/wp-content/uploads/2014/02/Gala-Image.jpg",
-  remote_audio_url: "http://res.cloudinary.com/dieter/video/upload/v1511434243/kgyrd5day1jkfru6qwb1.m4",
-  description: "http://pipelinetheatre.org/wp-content/uploads/2014/02/Gala-Image.jpg",
-  language: "English",
-  category: "Comedy",
-  lat:52.3600,
-  lng:4.8852
-  )
-
-smoking_policy = Story.create!(
-  user: bennie,
-  landmark: aepjen,
-  title: "Our solution to the smoking ban",
-  remote_image_url:  "https://at5.s3-eu-west-1.amazonaws.com/data/cache/at5/basedata/pf_image/1329473267-32ba582f.jpg",
-  remote_audio_url: "http://res.cloudinary.com/dieter/video/upload/v1511434243/kgyrd5day1jkfru6qwb1.m4",
-  description: "http://pipelinetheatre.org/wp-content/uploads/2014/02/Gala-Image.jpg",
-  language: "English",
-  category: "Comedy",
-  lat:52.3600,
-  lng:4.8852
-  )
-
-jongste_dief = Story.create!(
-  user: dwight,
-  landmark: indische_buurt,
-  title: "Youngest thief ever",
-  remote_image_url: "http://2.bp.blogspot.com/_MsXjKlVwry0/TM7IKkMR95I/AAAAAAAAAxI/WGhlmy6Bf5s/s1600/DSCF0138.JPG",
-  remote_audio_url: "http://res.cloudinary.com/dieter/video/upload/v1511434243/kgyrd5day1jkfru6qwb1.m4",
-  description: "Young shoplifter hoards candy",
-  language: "English",
-  category: "Informative",
-  lat:52.3600,
-  lng:4.8852
-  )
-
-conversations = Story.create!(
-  user: dwight,
-  landmark: indische_buurt,
-  title: "Converstations with neighbours",
-  remote_image_url: "https://www.vn.nl/wp-content/uploads/sites/3/2014/01/3e0e2f40-1355-4183-a1da-df7b2e136877_java12.jpg",
-  remote_audio_url: "http://res.cloudinary.com/dieter/video/upload/v1511434243/kgyrd5day1jkfru6qwb1.m4",
-  description: "Young shoplifter hoards candy",
-  language: "English",
-  category: "Informative",
-  lat:52.3600,
-  lng:4.8852
-  )
-
-trappist = Story.create!(
-  user: priest,
-  landmark: aepjen,
-  title: "Godly history of Trappist beer",
-  remote_image_url: "http://farm5.static.flickr.com/4019/4571490807_cc0d1c90ac.jpg",
-  remote_audio_url: "http://res.cloudinary.com/dieter/video/upload/v1511434243/kgyrd5day1jkfru6qwb1.m4",
-  description: "",
-  language: "English",
-  category: "History",
-  lat:52.3600,
-  lng:4.8852
-  )
-
-god = Story.create!(
-  user: priest,
-  landmark: oude_kerk,
-  title: "Fun talks with people about god, in a bar",
-  remote_image_url: "http://www.arjanbroers.nl/wp-content/uploads/2017/02/6-Mobile-pub-with-sister-Holkje-1024x683.jpg",
-  remote_audio_url: "http://res.cloudinary.com/dieter/video/upload/v1511434243/kgyrd5day1jkfru6qwb1.m4",
-  description: "Have you ever noticed there is a bar next to every church. There is a reason for that.",
-  language: "English",
-  category: "Informative",
-  lat:52.3600,
-  lng:4.8852
-  )
-
-interview1 = Story.create!(
-  user: sanne,
-  landmark: portuguese_synagogue,
-  title: "Interview met mijn oma over het leven in Amsterdam gedurende de 2e Wereld Oorlog",
-  remote_image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI1uRe3iX6kC_5yoQhXRLWJxmMlVLcfYSDyhdPWSord_9mH0Sg",
-  remote_audio_url: "http://res.cloudinary.com/dieter/video/upload/v1511434243/kgyrd5day1jkfru6qwb1.m4",
-  description: "Lang gesprek met oma over haar jeugd als een joods meisje in Amsterdam in de jaren 40",
-  language: "Dutch",
-  category: "History",
-  lat:52.3600,
-  lng:4.8852
-  )
-
-interview2 = Story.create!(
-  user: sanne,
-  landmark: jordaan,
-  title: "Gesprek met Sjonne, bekend van zijn straatkrantjes",
-  remote_image_url: "https://upload.wikimedia.org/wikipedia/commons/c/c6/Straatnieuws_seller_in_Zoetermeer_2008.jpg",
-  remote_audio_url: "http://res.cloudinary.com/dieter/video/upload/v1511434243/kgyrd5day1jkfru6qwb1.m4",
-  description: "Erg grappig gesprek met Sjonnie, deze man heeft wat meegemaakt",
-  language: "Dutch",
-  category: "Comedy",
-  lat:52.3600,
-  lng:4.8852
-  )
-
-interview3 = Story.create!(
-  user: sanne,
-  landmark: jordaan,
-  title: "In gesprek met een paar Marokaanse jongens in Amsterdam",
-  remote_image_url: "https://nos.nl/data/image/2016/03/26/268577/xxl.jpg",
-  remote_audio_url: "http://res.cloudinary.com/dieter/video/upload/v1511434243/kgyrd5day1jkfru6qwb1.m4",
-  description: "Leuk gesprek met een paar jongens en hun vriend uit Marokko",
-  language: "Dutch",
-  category: "Informative",
-  lat:52.3600,
-  lng:4.8852
-  )
-
-
-zwaarste_nacht_op_zee = Story.create!(
-  user: piet,
-  landmark: port_amsterdam,
-  title: "Just had the weirdest night at sea",
-  remote_image_url: "https://i.imgur.com/D63VN1z.jpg",
-  remote_audio_url: "http://res.cloudinary.com/dieter/video/upload/v1511434243/kgyrd5day1jkfru6qwb1.m4",
-  description: "30 years in this trade, all the latest instruments, and still I managed to get lost",
-  language: "English",
-  category: "Informative",
-  lat:52.3600,
-  lng:4.8852
-  )
-
-lewagon = Story.create!(
-  user: feiko,
-  landmark: alab,
-  title: "History of A-Lab",
-  remote_image_url: "https://www.dropbox.com/sh/g39jwm8xj3vk9uu/AAC21Y28YLXytDvdpk8hcmaca/color?dl=0&preview=_DSF0738.jpg",
-  remote_audio_url: "http://res.cloudinary.com/dieter/video/upload/v1511434243/kgyrd5day1jkfru6qwb1.m4",
-  description: "Short history of A-Lab, now a creative hub",
-  language: "English",
-  category: "Informative",
-  lat:52.3600,
-  lng:4.8852
-  )
 # ---------------------------------------------------------------------------------------------
 puts "Done!"
 
